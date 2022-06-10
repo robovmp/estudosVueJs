68ce7cc78f0314fcea204a767d918394ad52aaac
robovmp <mpviniciusmp@gmail.com>
2022-06-10 14:55:15 -0300

    dando inicio ao projeto TO-DO

---
+++ tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length" >
+            <task v-for="task in tasks" :key="task.name"
+            v-bind:task="task"></task>
+        </template>
+        <p class="no-task" v-else>Sua vida está em dia :-)</p>
+    </div>
+</template>
+
+<script>
+import Task from './TaskComp.vue'
+export default {
+    components:{Task},
+    props: {
+        tasks:{ type: Array, require: true }
+    }    
+}
+</script>
+
+<style>
+
+    .task-grid{
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .task-grid .task{
+        margin: 10px;
+    }
+
+    .no-task{
+        color: #aaa;
+        font-size: 1.7rem;
+    }
+
+</style>
